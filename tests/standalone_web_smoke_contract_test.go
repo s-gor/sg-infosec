013b776b96fca19aeaa3dad1f9c5a9f78e1ecdfb
Ser.Gor <293675787+s-gor@users.noreply.github.com>
2026-09-03 12:02:11 +0200

    test: align web smoke contract with direct admin bootstrap

--- tests/standalone_web_smoke_contract_test.go
+++ tests/standalone_web_smoke_contract_test.go
@@ -7,11 +7,16 @@ func TestStandaloneWebRealInstallSmokeIsInReleaseGate(t *testing.T) {
 	requireContains(t, smoke,
 		"install-standalone-web-from-github.sh",
 		"SG_INFOSEC_REPOSITORY_URL=\"file://$ROOT_DIR\"",
+		"12345678\\n12345678\\n",
+		"Login: admin",
 		"systemctl is-active --quiet sg-infosec-web.service",
-		"https://127.0.0.1:64443/infosec/setup",
 		"https://127.0.0.1:64443/infosec/login",
 		"https://127.0.0.1:64443/infosec/decisions",
 		"standalone web install smoke passed",
+	)
+	requireNotContains(t, smoke,
+		"/infosec/setup",
+		"One-time setup code",
 	)
 	workflow := readRepositoryFile(t, ".github/workflows/enforcer-gate.yml")
 	requireContains(t, workflow,
